b904efe2742bf0eef315b1fae85d2265d4425f2e
Simon A. F. Lund <os@safl.dk>
2025-06-13 12:50:12 +0200

    feat(nvme): add a mmio-helper for nvme
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- include/nvme.h
+++ include/nvme.h
@@ -1,13 +1,32 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) Simon Andreas Frimann Lund <os@safl.dk>
-/*
-	A collection of helpers for NVMe-controller operations via memory-mapped io
+//
 
-	All the functions rely on the availabilty of mmio-functions, specifically those defined by 	pci.h,
-	however, if you need a different implementation for the mmio operations that is given in pci.h,
-	then you just include the same function-signatures before
-*/
+/**
+ * Header-only NVMe MMIO access helpers
+ *
+ * This header provides simple helper functions for interacting with NVMe controller
+ * registers using memory-mapped I/O (MMIO). It assumes the availability of PCIe MMIO
+ * primitives defined in "pci.h".
+ *
+ * Custom MMIO support
+ * ===================
+ * If you want to use your own MMIO access functions instead of those in "pci.h", define
+ * the preprocessor symbol `UPCIE_NVME_PCI_INTERFACE` before including this header. Doing
+ * so will prevent inclusion of "pci.h", allowing your own definitions to take effect.
+ *
+ * Example:
+ *     #define UPCIE_NVME_PCI_INTERFACE
+ *     #include "my_mmio.h"
+ *     #include "nvme.h"
+ */
+#ifndef UPCIE_NVME_H
+#define UPCIE_NVME_H
+
+#ifndef UPCIE_NVME_PCI_INTERFACE
+#define UPCIE_NVME_PCI_INTERFACE
 #include <pci.h>
+#endif
 
 #define NVME_REG_CAP 0x00
 #define NVME_REG_VS 0x08
@@ -30,17 +49,17 @@
  *
  * - controller is disabled
  *
- * @param region A memory-mapped region pointing to the start of bar0
+ * @param bar0 A memory-mapped region pointing to the start of bar0
  * @param asq DMA-able address pointing to the start of the admin submission queue
  * @param acq DMA-able address pointing to the start of the admin completion queue
  *
  */
 static inline void
-nvme_controller_adminq_setup(void *region, uint64_t asq, uint64_t acq)
+nvme_controller_adminq_setup(void *bar0, uint64_t asq, uint64_t acq)
 {
-	pci_region_write32(region, NVME_REG_AQA, (0 << 16) | 0); // 1-entry queues
-	pci_region_write64(region, NVME_REG_ASQ, asq);
-	pci_region_write64(region, NVME_REG_ACQ, acq);
+	pci_region_write32(bar0, NVME_REG_AQA, (0 << 16) | 0); // 1-entry queues
+	pci_region_write64(bar0, NVME_REG_ASQ, asq);
+	pci_region_write64(bar0, NVME_REG_ACQ, acq);
 }
 
 /**
@@ -54,14 +73,14 @@ nvme_controller_adminq_setup(void *region, uint64_t asq, uint64_t acq)
  * - controller is disabled
  * - admin-queue properties have been setup
  *
- * @param region A memory-mapped region pointing to the start of bar0
+ * @param bar0 A memory-mapped region pointing to the start of bar0
  */
 static inline void
-nvme_controller_enable(uint8_t *region)
+nvme_controller_enable(uint8_t *bar0)
 {
 	uint32_t cc = (6 << 20) | (4 << 16) | 0x1;
 
-	pci_region_write32(region, NVME_REG_CC, cc);
+	pci_region_write32(bar0, NVME_REG_CC, cc);
 }
 
 /**
@@ -69,10 +88,14 @@ nvme_controller_enable(uint8_t *region)
  *
  * This is done via the memory-mapped NVME_REG_CSTS register pointed to by the given 'region'
  *
- * @param region A memory-mapped region pointing to the start of bar0
+ * @param bar0 A memory-mapped region pointing to the start of bar0
  */
+
+
 static inline int
-nvme_controller_is_enabled(void *region)
+nvme_controller_is_enabled(void *bar0)
 {
-	return pci_region_read32(region, NVME_REG_CSTS) & 1;
+	return pci_region_read32(bar0, NVME_REG_CSTS) & 1;
 }
+
+#endif ///< UPCIE_NVME_H
