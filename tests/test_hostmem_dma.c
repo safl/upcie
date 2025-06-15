29018e7e00aa4b4eb703821ccadde9df462da5df
Simon A. F. Lund <os@safl.dk>
2025-06-15 20:08:58 +0200

    tests(hostmem_dma): add check of alloc()/free()
    This is far from verifying that the DMA-allocator is actually DMA-able.
    However, to begin with, then verification that basic operations are
    functional, this is one them.
    
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ tests/test_hostmem_dma.c
@@ -0,0 +1,24 @@
+#include <hostmem.h>
+#include <hostmem_dma.h>
+#include <hostmem_heap.h>
+
+int
+main(int argc, const char *argv[])
+{
+	const size_t sizes[] = {1024, 1024 * 1024, 1024 * 1024 * 2ULL, 1024 * 1024 * 1024};
+	void *buf;
+
+	for (int i = 0; i < sizeof(sizes) / sizeof(*sizes); ++i) {
+		size_t nbytes = sizes[i];
+
+		buf = hostmem_dma_malloc(nbytes);
+		if (!buf) {
+			printf("hostmem_dma_malloc(%zu)", nbytes);
+			return -errno;
+		}
+
+		hostmem_dma_free(buf);
+	}
+
+	return 0;
+}
