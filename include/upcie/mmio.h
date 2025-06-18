7752c354e58a5dbe21432ce362032ce07546ba91
Simon A. F. Lund <os@safl.dk>
2025-06-18 16:00:01 +0200

    fix(mmio): split 64bit IO into two 32bit operations
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- include/upcie/mmio.h
+++ include/upcie/mmio.h
@@ -44,6 +44,8 @@ mmio_write32(void *region, uint32_t offset, uint32_t value)
 /**
  * Read a 64-bit value from an MMIO region at the given offset
  *
+ * NOTE: For "portability" then this splits the 64bit read into two 32bit reads
+ *
  * @param region  Pointer to the base of the MMIO region
  * @param offset  Byte offset from the base
  * @return        64-bit value read from the region
@@ -51,18 +53,24 @@ mmio_write32(void *region, uint32_t offset, uint32_t value)
 static inline uint64_t
 mmio_read64(void *region, uint32_t offset)
 {
-	return *(volatile uint64_t *)((uintptr_t)region + offset);
+	uint32_t lo = mmio_read32(region, offset + 0);
+	uint32_t hi = mmio_read32(region, offset + 4);
+
+	return ((uint64_t)hi << 32) | lo;
 }
 
 /**
  * Write a 64-bit value to an MMIO region at the given offset
+ *
+ * NOTE: For "portability" then this splits the 64bit write into two 32bit writes
  *
  * @param region  Pointer to the base of the MMIO region
  * @param offset  Byte offset from the base
  * @param value   64-bit value to write
  */
 static inline void
-mmio_write64(void *region, uint32_t offset, uint64_t value)
+mmio_write64(uint8_t *mmio, uint32_t offset, uint64_t val)
 {
-	*(volatile uint64_t *)((uintptr_t)region + offset) = value;
+	mmio_write32(mmio, offset + 0, (uint32_t)(val & 0xFFFFFFFF));
+	mmio_write32(mmio, offset + 4, (uint32_t)(val >> 32));
 }
