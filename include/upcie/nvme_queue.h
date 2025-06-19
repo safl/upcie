fe1b13bb4573141c8f154c2a6c6e754273780490
Simon A. F. Lund <os@safl.dk>
2025-06-19 09:57:04 +0200

    fix(nvme/queue): remove invalid db-assignment
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- include/upcie/nvme_queue.h
+++ include/upcie/nvme_queue.h
@@ -52,8 +52,6 @@ nvme_qp_init(struct nvme_qp *qp, uint32_t qid, uint16_t depth, struct nvme_contr
 	uint8_t dstrd_nbytes = 1U << (2 + nvme_reg_cap_get_dstrd(ctrlr->cap));
 	uint8_t *region = ctrlr->bar0;
 
-	qp->sqdb = region + 0x1000 + (2 * qid) * (1U << (2 + dstrd));
-	qp->cqdb = region + 0x1000 + (2 * qid * dstrd);
 	qp->sqdb = region + 0x1000 + (2 * qid) * dstrd_nbytes;
 	qp->cqdb = region + 0x1000 + (2 * qid + 1) * dstrd_nbytes;
 	qp->qid = qid;
