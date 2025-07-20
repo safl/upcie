157a72cc832257a441f2c65ab3ae22f7a8cabb04
Simon A. F. Lund <os@safl.dk>
2025-07-20 20:51:09 +0200

    refactor(example/nvme): separate initialization into RTE and NVMe
    Prepares for upcoming PRP pre-allocation on the queue. Improves
    readability and clarifies separation of concerns.
    
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- example/upcie_nvme_driver.c
+++ example/upcie_nvme_driver.c
@@ -4,13 +4,76 @@
 #define _UPCIE_WITH_NVME
 #include <upcie/upcie.h>
 
+struct rte {
+	struct hostmem_config config;
+	struct hostmem_heap heap;
+};
+
+struct nvme {
+	struct nvme_controller ctrlr;
+	struct nvme_qpair ioq;
+};
+
+int
+rte_init(struct rte *rte)
+{
+	int err;
+
+	err = hostmem_config_init(&rte->config);
+	if (err) {
+		printf("FAILED: hostmem_config_init(); err(%d)\n", err);
+		return err;
+	}
+
+	err = hostmem_heap_init(&rte->heap, 1024 * 1024 * 128ULL, &rte->config);
+	if (err) {
+		printf("FAILED: hostmem_heap_init(); err(%d)\n", err);
+		return -err;
+	}
+
+	return 0;
+}
+
+int
+nvme_init(struct nvme *nvme, const char *bdf, struct rte *rte)
+{
+	struct nvme_completion cpl = {0};
+	struct nvme_command cmd = {0};
+	int err;
+
+	err = nvme_controller_open(&nvme->ctrlr, bdf, &rte->heap);
+	if (err) {
+		printf("FAILED: nvme_device_open(); err(%d)\n", err);
+		return -err;
+	}
+
+	cmd.opc = 0x6; ///< IDENTIFY
+	cmd.prp1 = hostmem_dma_v2p(&rte->heap, nvme->ctrlr.buf);
+	cmd.cdw10 = 1; // CNS=1: Identify Controller
+
+	err = nvme_qpair_submit_sync(&nvme->ctrlr.aq, &cmd, nvme->ctrlr.timeout_ms, &cpl);
+	if (err) {
+		printf("FAILED: nvme_qpair_submit_sync(); err(%d)\n", err);
+		nvme_controller_close(&nvme->ctrlr);
+		return err;
+	}
+
+	printf("SN('%.*s')\n", 20, ((uint8_t *)nvme->ctrlr.buf) + 4);
+	printf("MN('%.*s')\n", 40, ((uint8_t *)nvme->ctrlr.buf) + 24);
+
+	err = nvme_controller_create_io_qpair(&nvme->ctrlr, &nvme->ioq, 32);
+	if (err) {
+		printf("FAILED: nvme_device_create_io_qpair(); err(%d)\n", err);
+	}
+
+	return 0;
+}
+
 int
 main(int argc, char **argv)
 {
-	struct hostmem_config config = {0};
-	struct hostmem_heap heap = {0};
-	struct nvme_controller ctrlr = {0};
-	struct nvme_qpair ioq = {0};
+	struct nvme nvme = {0};
+	struct rte rte = {0};
 	int err;
 
 	if (argc != 2) {
@@ -18,49 +81,20 @@ main(int argc, char **argv)
 		return 1;
 	}
 
-	err = hostmem_config_init(&config);
+	err = rte_init(&rte);
 	if (err) {
-		return err;
-	}
-
-	err = hostmem_heap_init(&heap, 1024 * 1024 * 128ULL, &config);
-	if (err) {
-		printf("FAILED: hostmem_heap_init(); err(%d)\n", err);
+		printf("FAILED: rte_init();");
 		return -err;
 	}
 
-	err = nvme_controller_open(&ctrlr, argv[1], &heap);
+	err = nvme_init(&nvme, argv[1], &rte);
 	if (err) {
-		printf("FAILED: nvme_device_open(); err(%d)\n", err);
+		printf("FAILED: nvme_init();");
 		return -err;
 	}
 
-	{
-		struct nvme_command cmd = {0};
-		struct nvme_completion cpl = {0};
-
-		cmd.opc = 0x6; ///< IDENTIFY
-		cmd.prp1 = hostmem_dma_v2p(&heap, ctrlr.buf);
-		cmd.cdw10 = 1; // CNS=1: Identify Controller
-
-		err = nvme_qpair_submit_sync(&ctrlr.aq, &cmd, ctrlr.timeout_ms, &cpl);
-		if (err) {
-			printf("FAILED: nvme_qpair_submit_sync(); err(%d)\n", err);
-			goto exit;
-		}
-	}
-
-	printf("SN('%.*s')\n", 20, ((uint8_t *)ctrlr.buf) + 4);
-	printf("MN('%.*s')\n", 40, ((uint8_t *)ctrlr.buf) + 24);
-
-	err = nvme_controller_create_io_qpair(&ctrlr, &ioq, 32);
-	if (err) {
-		printf("FAILED: nvme_device_create_io_qpair(); err(%d)\n", err);
-	}
-
-exit:
-	nvme_controller_close(&ctrlr);
-	hostmem_heap_term(&heap);
+	nvme_controller_close(&nvme.ctrlr);
+	hostmem_heap_term(&rte.heap);
 
 	return err;
 }
