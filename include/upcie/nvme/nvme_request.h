6e437316d970697944d7d79ef02ae2b5888ee6c7
Simon A. F. Lund <os@safl.dk>
2025-07-21 08:50:24 +0200

    feat(nvme/request): add helper nvme_request_prep_command_prps_contig()
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- include/upcie/nvme/nvme_request.h
+++ include/upcie/nvme/nvme_request.h
@@ -149,3 +149,50 @@ nvme_request_get(struct nvme_request_pool *pool, uint16_t cid)
 	assert(cid < NVME_REQUEST_POOL_LEN);
 	return &pool->reqs[cid];
 }
+
+/**
+ * Prepare the PRP list for a command with a contiguous data buffer.
+ *
+ * This function initializes the Physical Region Page (PRP) entries in the given NVMe command
+ * (`cmd`) using the provided request and a contiguous data buffer (in VA-space).
+ * It sets up the PRP1 and PRP2 fields in the command to describe the physical memory backing the
+ * `data` buffer, allowing the NVMe controller to access the buffer during command execution.
+ *
+ * Caveats
+ * -------
+ *
+ * - Assumes that the memory backing `dbuf` in `heap` is physically contiguous.
+ * - Does *not* support PRP list chaining; only a single list page is constructed.
+ *
+ * @param request Pointer to the NVMe request context used for tracking and metadata.
+ * @param heap Pointer to the hostmemory heap that dbuf is allocated within.
+ * @param dbuf Pointer to the contiguous data buffer to be described by PRPs.
+ * @param dbuf_nbytes Size in bytes of the data buffer.
+ * @param cmd Pointer to the NVMe command to be prepared with PRP entries.
+ */
+static inline void
+nvme_request_prep_command_prps_contig(struct nvme_request *request, struct hostmem_heap *heap,
+				      void *dbuf, size_t dbuf_nbytes, struct nvme_command *cmd)
+{
+	const uint64_t npages = dbuf_nbytes >> heap->config->pagesize_shift;
+	const uint64_t pagesize = heap->config->pagesize;
+
+	/* Chaining is not supported, thus assert that the given dbuf fits. */
+	assert(npages <= 1 + 512);
+
+	cmd->prp1 = hostmem_dma_v2p(heap, dbuf);
+
+	if (npages == 1) {
+		return;
+	} else if (npages == 2) {
+		cmd->prp2 = hostmem_dma_v2p(heap, dbuf + pagesize);
+	} else {
+		uint64_t *prp_list = request->prp;
+
+		cmd->prp2 = request->prp_addr;
+		for (uint64_t i = 1; i < npages; ++i) {
+
+			prp_list[i - 1] = cmd->prp1 + (i << heap->config->pagesize_shift);
+		}
+	}
+}
